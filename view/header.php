7c56de3e2b1f328822b26c39a714c651e634120a
dimamedia <web@dima.fi>
2014-02-17 17:55:27 +0200

    Update header.php

--- view/header.php
+++ view/header.php
@@ -12,7 +12,7 @@ print <<< html
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="$title">
-    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+    <link rel="shortcut icon" href="/favicon.png">
 
     <title>$title</title>
 
