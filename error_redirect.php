718073d87a1f697ffbb34846d0a9ee401004728e
dimamedia <web@dima.fi>
2014-02-17 17:38:52 +0200

    Update error_redirect.php

--- error_redirect.php
+++ error_redirect.php
@@ -1,10 +1,10 @@
 <?php
 require("config.php");
-require("view/header.php");
+require(VIEW."/header.php");
 
-if($_SERVER['QUERY_STRING'] == "404") require("view/404.php");
-elseif($_SERVER['QUERY_STRING'] == "500") require("view/500.php");
+if($_SERVER['QUERY_STRING'] == "404") require(VIEW."/404.php");
+elseif($_SERVER['QUERY_STRING'] == "500") require(VIEW."/500.php");
 
-require("view/footer.php");
+require(VIEW."/footer.php");
 
 ?>
