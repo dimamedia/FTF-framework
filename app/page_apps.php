0a6073fb29916fd0798975dfb360e28bb3ff06d8
Dima Tsvetkov <web@dima.fi>
2014-02-18 09:44:16 +0200

    Update page_apps.php

--- app/page_apps.php
+++ app/page_apps.php
@@ -5,7 +5,7 @@ function jumbotron() {
 	print <<< html
       <div class="jumbotron">
         <h2>Fuck The Frameworks <small>-framework</small></h2>
-        <p class="lead">Your code. Your design. No dependency.</p>
+        <p class="lead">Your code. Your design. No dependencies.</p>
         <p><a class="btn btn-lg btn-success" href="https://github.com/dimamedia/ftf-framework/archive/master.zip" role="button"><span class="glyphicon glyphicon glyphicon-download"></span> Download</a></p>
       </div>
 html;
